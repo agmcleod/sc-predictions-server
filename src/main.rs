cf9d65f2079bafb87d513c31747ff6923c1fb9b1
Aaron McLeod <aaron.g.mcleod@gmail.com>
2018-10-14 18:49:29 -0400

    server running

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 extern crate actix_web;
-use actix_web::{http, server, App, Path, Responder};
+extern crate env_logger;
+use actix_web::{http, server, App, Path, Responder, middleware::Logger};
 
 fn index(info: Path<(u32, String)>) -> impl Responder {
     format!("Hello {}! id: {}", info.1, info.0)
 }
 
 fn main() {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     server::new(|| {
         App::new()
+            .middleware(Logger::default())
+            .middleware(Logger::new("%a %{User-Agent}i"))
             .route("/{id}/{name}", http::Method::GET, index)
     })
-    .bind("8080").expect("Can not bind to 127.0.0.1:8080")
+    .bind("0.0.0.0:8080").expect("Can not bind to :8080")
     .run();
 }
